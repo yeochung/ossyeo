bb84cde7d785ab6406cf0179d668b9c75a1b63fd
yeochung <yeochung37@gmail.com>
2019-05-30 10:34:43 +0900

    Created C files

---
+++ main (3).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void sub1() {
+	for (int i = 0; i<5; i++)
+		printf("*\n");
+}
+
+void sub2(){
+	printf("Mission Clear!!!!"\n);
+}
+
+int main () {
+	sub1();
+	sub2();
+	return 0;
+}
